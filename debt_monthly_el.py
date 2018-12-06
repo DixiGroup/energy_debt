b4deb25fd7d1ca9f8b83aece80ba4ef351c21798
oviedrov <viedrov.chesno@gmail.com>
2018-12-06 13:04:13 +0200

    oblast var added for spreadsheets with correctly named sheets

--- debt_monthly_el.py
+++ debt_monthly_el.py
@@ -1,6 +1,9 @@
 ﻿import os
 import pandas as pd
 
+os.chdir('C:\\Users\\apelianer\\Desktop\\Dixi\\debt_monthly')
+wd = os.getcwd()
+
 # lookup for months
 months_ukr = ["січень", "лютий", "березень", "квітень", "травень", "червень",
               "липень", "серпень", "вересень", "жовтень", "листопад", "грудень"]
@@ -21,13 +24,15 @@ def parse_single_sheet(xlfile, sheet_name, energy_type = 'el'):
                       'paid_month_money_tuah', 'debt_correction_tuah',
                       'debt_month_end']
     
-    # get month and company
+    # get month, company, and oblast
     
     current_month_index = debtdf.loc[pd.notnull(debtdf['consumer_type']) & debtdf['consumer_type'].str.contains('року')].index[0]
     current_month = debtdf['consumer_type'][current_month_index].replace(' за ', '').replace(' року', '')
     current_month
     
     company = debtdf['consumer_type'][current_month_index+1]
+    
+    oblast = sheet_name.split(sep = '_')[0]
     
     # remove unnecessary rows after heat (get second index for Споживачі)
     
@@ -99,13 +104,14 @@ def parse_single_sheet(xlfile, sheet_name, energy_type = 'el'):
          'інші споживачі']
     
     # assign company and month vars
-    debtdf_el = debtdf_el.assign(company = company, 
+    debtdf_el = debtdf_el.assign(company = company,
+                                 oblast = oblast,
                                  year = str.split(current_month, sep = ' ')[1],
                                  month = int(months_lookup['months_dig'][months_lookup['months_ukr'] == str.split(current_month, sep = ' ')[0]]))
     
     # move newly created vars forward / rearrange
     cols = debtdf_el.columns.tolist()
-    cols = cols[-2:]+cols[-3:-2]+cols[-4:-3]+cols[:-4]
+    cols = cols[-2:]+cols[-3:-2]+cols[-4:-3]+cols[-5:-4]+cols[:-5]
     debtdf_el = debtdf_el[cols]
     
     # repeat for heat df (attention, other indices)
@@ -131,10 +137,11 @@ def parse_single_sheet(xlfile, sheet_name, energy_type = 'el'):
     company = company_heat
     del(company_heat)
     debtdf_heat = debtdf_heat.assign(company = company, 
+                                     oblast = oblast,
                                      year = str.split(current_month, sep = ' ')[1],
                                      month = int(months_lookup['months_dig'][months_lookup['months_ukr'] == str.split(current_month, sep = ' ')[0]]))
     cols = debtdf_heat.columns.tolist()
-    cols = cols[-2:]+cols[-3:-2]+cols[-4:-3]+cols[:-4]
+    cols = cols[-2:]+cols[-3:-2]+cols[-4:-3]+cols[-5:-4]+cols[:-5]
     debtdf_heat = debtdf_heat[cols]
     
     if(energy_type == 'el'):
