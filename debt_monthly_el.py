c2966051cf3e15be07fc6539e50ec0040456957d
oviedrov <viedrov.chesno@gmail.com>
2018-12-11 18:46:28 +0200

    row numbers instead of strings as anchors; more extensive filtering of spreadsheet names

--- debt_monthly_el.py
+++ debt_monthly_el.py
@@ -28,44 +28,21 @@ def parse_single_sheet(xlfile, sheet_name, energy_type = 'el'):
     current_month
     
     company = debtdf['consumer_type'][current_month_index+1]
-    
+    if(pd.isnull(company)):
+        company = debtdf['consumer_type'].iloc[10]
+
     oblast = sheet_name.split(sep = '_')[0]
     
     # remove unnecessary rows after heat (get second index for Споживачі)
     
     debtdf = debtdf.reset_index(drop = True)
-    index_trim1 = debtdf['consumer_type'][debtdf['consumer_type'] == 'Споживачі'].index[0]
-    index_trim2 = debtdf['consumer_type'][debtdf['consumer_type'] == 'Споживачі'].index[1]
-    debtdf = debtdf.iloc[(index_trim1+1):index_trim2,:]
      
-    # split into el and heat (get second index for Всього)
+    # slice relevant rows
     
-    debtdf = debtdf.reset_index(drop = True)
-    firstcol = list(debtdf['consumer_type'])
-    idx_total = []
-    for idx, val in enumerate(firstcol):
-        if('Всього' in str(val)):
-            idx_total = idx_total + [idx-1]
-    
-    if(pd.isnull(company)):
-        company = debtdf['consumer_type'][idx_total[0]]
-    
-    if(pd.notnull(debtdf['consumer_type'][idx_total[1]])):
-        company_heat = debtdf['consumer_type'][idx_total[1]]
-    else:
-        company_heat = company
-    
-    idx_to_split = idx_total[1]+1
-    
-    debtdf_el = debtdf.iloc[:idx_to_split,:]
-    
+    debtdf_el = debtdf.iloc[13:32,:]
+    debtdf_el = debtdf_el.loc[~debtdf_el['consumer_type'].str.contains('тому числі')]
+
     # filter redundant rows & create general consumer category var
-    debtdf_el = debtdf_el[debtdf_el['consumer_type'].notnull()]
-    debtdf_el = debtdf_el[debtdf_el['consumer_type'] != 1]
-    debtdf_el = debtdf_el[debtdf_el['consumer_type'] != company]
-    debtdf_el = debtdf_el[debtdf_el['consumer_type'] != company_heat]
-    debtdf_el = debtdf_el[~debtdf_el['consumer_type'].str.contains("сього|тому числі", na = False)]
-    
     debtdf_el.loc[debtdf_el['consumer_type'].str.contains("\d."), 'consumer_cat'] = debtdf_el.loc[debtdf_el['consumer_type'].str.contains("\d."), 'consumer_type']    
     debtdf_el['consumer_cat'] = debtdf_el['consumer_cat'].ffill().str.replace('\d.|,', '').str.lower().str.strip() 
     debtdf_el = debtdf_el[debtdf_el['consumer_type'] != '1.Промисловість']
@@ -116,11 +93,12 @@ def load_all_sheets(filename):
     filepath = os.path.join('raw data', filename)
     xlfile = pd.ExcelFile(filepath)
     sheet_names_series = pd.Series(xlfile.sheet_names)
-    cond = ~sheet_names_series.str.contains('!|сього')
+    cond = ~sheet_names_series.str.contains('!|сього|ТЕЦ|тепло|_оплата')
     sheet_names_to_read = sheet_names_series[cond][:-1]
     
     list_ = []
     for sheet in sheet_names_to_read:
+        print(sheet)
         df = parse_single_sheet(xlfile, sheet)
         list_.append(df)
     frame = pd.concat(list_)
@@ -131,11 +109,14 @@ raw_data_files = os.listdir('raw data')
 
 list_el = []
 for fi in raw_data_files:
+    print(fi)
     df = load_all_sheets(fi)
     list_el.append(df)
-frame_el = pd.concat(list_el).sort_values(by = ['year', 'month'])
 
-frame_el_only = frame_el.loc[~frame_el.company.str.contains('ТЕЦ|Київтепло'), :]
+frame_el = pd.concat(list_el).sort_values(by = ['year', 'month'])
+frame_el['company'][frame_el['company'].isnull()] = frame_el['oblast'][frame_el['company'].isnull()]
+
+frame_el_only = frame_el.loc[~frame_el['company'].str.contains('ТЕЦ|тепло')]
 
 # create folder open data if it doesn't exist
 od_folder = 'open data' 
